2edc8ed45a35dc53c1b2edca1b5c2917cc81c92c
CaraCentral <cararoche192@gmail.com>
2024-08-21 15:53:49 -0400

    Fixing for Deployment

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Project } from '../_models/project';
+import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag';
 
 @Component({
@@ -11,8 +11,9 @@ import { Tag } from '../_models/Tag';
 export class PortfolioComponent {
   
   projects: Project[] = [
-    {id:0, name: "Web Portfolio", pictures: [], projectLink: "", summary:"A website to show off my expertise.", description: "uhhh, yeah what I said", tags: [Tag.ANGULAR, Tag.TYPESCRIPT]}
-    
+    {id:0, name: "Web Portfolio", pictures: [], projectLink: "", summary:"A website to show off my expertise.", description: "uhhh, yeah what I said", tags: [Tag.ANGULAR, Tag.TYPESCRIPT]},
+    {id:1, name: "Review Website", pictures: [], projectLink: "", summary:"A website for my video game reviews.", description: "uhhh, yeah what I said", tags: [Tag.ANGULAR, Tag.TYPESCRIPT]},
+
   ];
 
   constructor(private titleService: Title){
